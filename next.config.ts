245ee7997e73b60b64ed2dfaf92b09df356e1104
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-12-18 00:44:47 +0500

    Testimonials

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,29 @@
+
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: '**.com',
-            port: '',
-        },
-    ],
-}
+    /* config options here */
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: "**",
+                port: ''
+            },
+            {
+                protocol: 'http',
+                hostname: "**.com",
+                port: ''
+            },
+        ]
+    },
 };
 
 export default nextConfig;
